5ee2b6469b9feabd19e6fef637e9de57a4826e5c
Santoshi Chaulagain <santoshichaulagain57@gmail.com>
2023-08-21 22:35:06 +0545

    changes

---
+++ src/Components/PageWrapper.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+class PageWrapper extends Component{
+ render(){
+
+    return (
+        <div>
+           <nav className="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            
+            <div className="container">
+                <a classname="navbar-brand js-scroll-trigger" href="#page-top">Start Bootstrap</a>
+            </div>
+            </nav> 
+        </div>
+    )
+ }
+
+}
+
+export default PageWrapper;
